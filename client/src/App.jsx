95491cbd625b87cb387329b505b2a5480234485b
nishmitha <nishkallaje@gmail.com>
2025-06-03 16:44:57 +0530

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+
 
 const App = () => {
   return <BrowserRouter>
@@ -18,6 +20,7 @@ const App = () => {
     <Route path='/signup' element={<SignUp/>}/>
     <Route element={<PrivateRoute />}>
               <Route path='/profile' element={<Profile />} />
+              <Route path='/create-listing' element={< CreateListing/>} />
               </Route>
   </Routes>
   
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ import {
   deleteUserStart,
   deleteUserSuccess,
  signOutUserStart,} from '../redux/user/userSlice';
+ import { Link } from 'react-router-dom';
   import { useDispatch } from 'react-redux';
 
 
@@ -186,6 +187,13 @@ return (
           disabled={loading}
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >update</button>
+
+        <Link
+                  className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+                  to={'/create-listing'}
+                >
+                  Create Listing
+                </Link>
 
 
  </form>
